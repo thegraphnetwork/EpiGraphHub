64cce8745520b7accdf2395b99a64175e397da56
Eduardo Correa Araujo <eduardocorreaaraujo8@gmail.com>
2022-02-01 10:03:29 -0300

    Updated colombia_data.py with the changes requested

--- Data_Collection/CRON_scripts/colombia_data.py
+++ Data_Collection/CRON_scripts/colombia_data.py
@@ -16,36 +16,22 @@ from sodapy import Socrata
 
 client = Socrata("www.datos.gov.co", '078u4PCGpnDfH157kAkVFoWea')
 
+def chunked_fetch(start, chunk_size, maxrecords):
+    
+    slice_date = datetime.date(datetime.today()) - timedelta(200)
 
-def load_into_db(client):
-    # count the total number of records in the dataframe
-    records = client.get_all("gt2j-8ykr", select = 'COUNT(*)')
-    for i in records:
-        record_count = i
-        break
+    slice_date = slice_date.strftime('%Y-%m-%d')
     
     # start the looping to get the data in chunks of 10000 rows 
     start = 0             # Start at 0
     chunk_size = 10000     # Fetch 10000 rows at a time
-    
-    '''
-    date that separate the data that will be ignored and the data that will be 
-    updated in the param if_row_exists of the function upsert
-    (only will be updated the data related with the last 3 months)
-    '''
-    
-    today = datetime.date(datetime.today())
-    days_ago = timedelta(90)
-    slice_date = today - days_ago
-    slice_date = slice_date.strftime('%Y-%m-%d')
-    
-    
-    while True:
-        # Fetch the set of records starting at 'start'
-        results =  client.get("gt2j-8ykr", offset=start, limit=chunk_size) 
         
+    while start < maxrecords:
+        
+        # Fetch the set of records starting at 'start'
         # create a df with this chunk files
-        df_new = pd.DataFrame.from_records(results)
+        df_new = pd.DataFrame.from_records(client.get("gt2j-8ykr", offset=start, limit=chunk_size,
+                                    order = 'fecha_reporte_web', where = f'fecha_reporte_web > "{slice_date}"'))
         df_new.index.name = 'id_'
         
         df_new = df_new.convert_dtypes()
@@ -54,31 +40,34 @@ def load_into_db(client):
         df_new['fecha_reporte_web'] = pd.to_datetime(df_new['fecha_reporte_web'], errors='coerce')
         df_new['fecha_de_notificaci_n'] = pd.to_datetime(df_new['fecha_de_notificaci_n'], errors='coerce')
         
-        # separate the part of the df that will be set as `ignore` and `update` in the if_row_exists params. 
-        df_new1 = df_new.loc[df_new['fecha_reporte_web'] <= slice_date]
-        df_new2 = df_new.loc[df_new['fecha_reporte_web'] > slice_date] 
+        # Move up the starting record
+        start = start + chunk_size
         
-        del df_new 
+        yield df_new
         
+
+def load_into_db(client):
+    # count the total number of records in the dataframe
+    records = client.get_all("gt2j-8ykr", select = 'COUNT(*)')
+    for i in records:
+        record_count = i
+        break
+    
+    del records
+    
+    start = 0
+    chunk_size = 10000
+    maxrecords = int(record_count['COUNT'])
+    
+    for df_new in chunked_fetch(start, chunk_size, maxrecords):
+        # separate the part of the df that will be set as `ignore` and `update` in the if_row_exists params.
+ 
         # put the data into the bank
         engine = create_engine('postgresql://epigraph:epigraph@localhost:5432/epigraphhub')
         with engine.connect() as conn:
-            upsert(con=conn, df = df_new1, table_name='casos_positivos_covid', schema='colombia', if_row_exists='ignore',
+            upsert(con=conn, df = df_new, table_name='casos_positivos_covid', schema='colombia', if_row_exists= 'update',
                 chunksize=1000, add_new_columns=True, create_table=True) 
             
-        with engine.connect() as conn:
-            upsert(con=conn, df = df_new2, table_name='casos_positivos_covid', schema='colombia', if_row_exists='update',
-                chunksize=1000, add_new_columns=True, create_table=True) 
-            
-        del df_new1
-        del df_new2
-    
-        # Move up the starting record
-        start = start + chunk_size
-        # If we have fetched all of the records, bail out
-        if (start > int(record_count['COUNT'])):
-            break
-        #break
         
 if __name__ == "__main__":
     start_time = time.time()
