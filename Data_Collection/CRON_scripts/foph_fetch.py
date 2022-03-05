14c5aae638b2cf473bf2a7bb24d130f3bb91c007
Eduardo Correa Araujo <eduardocorreaaraujo8@gmail.com>
2022-03-04 22:00:26 -0300

    I made some corrections to put all the columns name in the lower case and put the name in the format combined

--- Data_Collection/CRON_scripts/foph_fetch.py
+++ Data_Collection/CRON_scripts/foph_fetch.py
@@ -15,6 +15,7 @@ TABLES = context['sources']['individual']['csv']['daily']
 
 def load_into_db(table, url, log=True):
     new_df = pd.read_csv(url)
+    new_df = new_df.rename(columns = str.lower)
     new_df.index.name = 'id_'
     if not 'date' in new_df.columns:
         new_df['date'] = pd.to_datetime(new_df.datum)
@@ -25,7 +26,7 @@ def load_into_db(table, url, log=True):
     
     engine = create_engine(config.DB_URI)
     with engine.connect() as conn:
-        upsert(con=conn, df=new_df, table_name=f'foph_{table.lower()}', schema='switzerland', if_row_exists='update',
+        upsert(con=conn, df=new_df, table_name=f'foph_{table.lower()}_d', schema='switzerland', if_row_exists='update',
             chunksize=1000, add_new_columns=True, create_table=True) 
     if log:
         logger.info(f'Table {table} updated')
