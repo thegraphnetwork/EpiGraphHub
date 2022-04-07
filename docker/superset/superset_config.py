535463433f375fe326b33cb1305b35b67362fe85
Flávio Codeço Coelho <fccoelho@gmail.com>
2022-04-07 16:37:27 +0200

    Added support to translations

--- docker/superset/superset_config.py
+++ docker/superset/superset_config.py
@@ -91,3 +91,29 @@ class CeleryConfig:
 
 CELERY_CONFIG = CeleryConfig
 SQLALCHEMY_DATABASE_URI = "sqlite:////opt/data/superset/superset.db"
+
+
+# ---------------------------------------------------
+# Babel config for translations
+# ---------------------------------------------------
+# Setup default language
+BABEL_DEFAULT_LOCALE = "en"
+# Your application default translation path
+BABEL_DEFAULT_FOLDER = "superset/translations"
+# The allowed translation for you app
+LANGUAGES = {
+    "en": {"flag": "us", "name": "English"},
+    # "es": {"flag": "es", "name": "Spanish"},
+    # "it": {"flag": "it", "name": "Italian"},
+    "fr": {"flag": "fr", "name": "French"},
+    # "zh": {"flag": "cn", "name": "Chinese"},
+    # "ja": {"flag": "jp", "name": "Japanese"},
+    # "de": {"flag": "de", "name": "German"},
+    "pt": {"flag": "pt", "name": "Portuguese"},
+    "pt_BR": {"flag": "br", "name": "Brazilian Portuguese"},
+    # "ru": {"flag": "ru", "name": "Russian"},
+    # "ko": {"flag": "kr", "name": "Korean"},
+    # "sk": {"flag": "sk", "name": "Slovak"},
+    # "sl": {"flag": "si", "name": "Slovenian"},
+    # "nl": {"flag": "nl", "name": "Dutch"},
+}
