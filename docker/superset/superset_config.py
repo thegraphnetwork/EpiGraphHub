9f69a971ae5ee8c38ef0a7b7d7bdf108e09fc761
Flávio Codeço Coelho <fccoelho@gmail.com>
2022-05-10 15:11:45 +0200

    added more languages (#91)

--- docker/superset/superset_config.py
+++ docker/superset/superset_config.py
@@ -103,19 +103,19 @@ BABEL_DEFAULT_FOLDER = "superset/translations"
 # The allowed translation for you app
 LANGUAGES = {
     "en": {"flag": "us", "name": "English"},
-    # "es": {"flag": "es", "name": "Spanish"},
-    # "it": {"flag": "it", "name": "Italian"},
+    "es": {"flag": "es", "name": "Spanish"},
+    "it": {"flag": "it", "name": "Italian"},
     "fr": {"flag": "fr", "name": "French"},
-    # "zh": {"flag": "cn", "name": "Chinese"},
-    # "ja": {"flag": "jp", "name": "Japanese"},
-    # "de": {"flag": "de", "name": "German"},
+    "zh": {"flag": "cn", "name": "Chinese"},
+    "ja": {"flag": "jp", "name": "Japanese"},
+    "de": {"flag": "de", "name": "German"},
     "pt": {"flag": "pt", "name": "Portuguese"},
     "pt_BR": {"flag": "br", "name": "Brazilian Portuguese"},
     # "ru": {"flag": "ru", "name": "Russian"},
-    # "ko": {"flag": "kr", "name": "Korean"},
+    "ko": {"flag": "kr", "name": "Korean"},
     # "sk": {"flag": "sk", "name": "Slovak"},
     # "sl": {"flag": "si", "name": "Slovenian"},
-    # "nl": {"flag": "nl", "name": "Dutch"},
+    "nl": {"flag": "nl", "name": "Dutch"},
 }
 
 # EMAIL
