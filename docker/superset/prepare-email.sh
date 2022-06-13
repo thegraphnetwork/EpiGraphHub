cd9cd14e63f8aeb84045380d8041232c4aa940c4
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-06-13 17:21:15 -0400

    Fix smtp script

--- docker/superset/prepare-email.sh
+++ docker/superset/prepare-email.sh
@@ -2,17 +2,16 @@
 
 set -ex
 
-# POSTFIX_MAIN=/etc/postfix/main.cf
-POSTFIX_MAIN=/tmp/log.txt
-GMAIL_PATTERN="relayhost = [smtp.gmail.com]:587"
-grep "$GMAIL_PATTERN" $POSTFIX_MAIN
+POSTFIX_MAIN=/etc/postfix/main.cf
 
-cat >> $POSTFIX_MAIN <<- POSTFIX
+# make a copy
+cp "${POSTFIX_MAIN}" "${POSTFIX_MAIN}-$(date --iso-8601)"
+
+cat >> ${POSTFIX_MAIN} <<- POSTFIX
 # new changes - 2022-04-08
-# ref: https://app.mailjet.com/auth/get_started/developer
 # ref: https://www.linuxfordevices.com/tutorials/ubuntu/smtp-relay-on-ubuntu
 # ref: https://linuxscriptshub.com/configure-smtp-with-gmail-using-postfix/
-relayhost = [smtp.gmail.com]:587
+relayhost = [${SUPERSET_MAIL_SERVER}]:${SUPERSET_MAIL_PORT}
 smtp_use_tls = yes
 smtp_sasl_auth_enable = yes
 smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd
@@ -20,7 +19,14 @@ smtp_sasl_security_options =
 smtp_tls_CAfile = /etc/ssl/certs/ca-certificates.crt
 POSTFIX
 
-cat /etc/postfix/sasl_passwd
+POSTFIX_SASL_PASSWD=/etc/postfix/sasl_passwd
+# make a copy
+cp "${POSTFIX_SASL_PASSWD}" "${POSTFIX_SASL_PASSWD}-$(date --iso-8601)"
 
+cat >> ${POSTFIX_SASL_PASSWD} <<- POSTFIX
+[${SUPERSET_MAIL_SERVER}]:${SUPERSET_MAIL_PORT} ${SUPERSET_MAIL_USERNAME}:${SUPERSET_MAIL_PASSWORD}
+POSTFIX
 
-# POSTFIX_MAIN=/etc/postfix/main.cf
+postmap ${POSTFIX_SASL_PASSWD}
+
+service postfix restart
