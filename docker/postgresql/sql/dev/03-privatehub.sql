cc568d8f54689dc9261e5fe0993cd24a5594ec2d
Sandro Loch <es.loch@gmail.com>
2022-08-24 10:25:16 -0300

    fix(postgres): Extension to postgreSQL script (#1)
    * chore(docker): update plpython3u extension.
    
    * chore(postgres): add postgis_raster extension.
    
    * chore(docker): set port for external access to postgres
    
    * chore(postgres): add database connection statement for each script

--- docker/postgresql/sql/dev/03-privatehub.sql
+++ docker/postgresql/sql/dev/03-privatehub.sql
@@ -5,6 +5,8 @@
 -- Dumped from database version 10.19 (Ubuntu 10.19-0ubuntu0.18.04.1)
 -- Dumped by pg_dump version 10.19 (Ubuntu 10.19-0ubuntu0.18.04.1)
 
+\c dev_privatehub
+
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -44,6 +46,79 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 --
 
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+--
+-- Name: plpython3u; Type: EXTENSION; Schema: -; Owner:
+--
+
+CREATE EXTENSION IF NOT EXISTS plpython3u WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpython3u; Type: COMMENT; Schema: -; Owner:
+--
+
+COMMENT ON EXTENSION plpython3u IS 'PL/Python3U untrusted procedural language';
+
+
+--
+-- Name: plpython3u; Type: EXTENSION; Schema: -; Owner:
+--
+
+CREATE EXTENSION IF NOT EXISTS plpython3u WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpython3u; Type: COMMENT; Schema: -; Owner:
+--
+
+COMMENT ON EXTENSION plpython3u IS 'PL/PythonU untrusted procedural language';
+
+
+--
+-- Name: plr; Type: EXTENSION; Schema: -; Owner:
+--
+
+CREATE EXTENSION IF NOT EXISTS plr WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION plr; Type: COMMENT; Schema: -; Owner:
+--
+
+COMMENT ON EXTENSION plr IS 'load R interpreter and execute R script from within a database';
+
+
+--
+-- Name: postgis; Type: EXTENSION; Schema: -; Owner:
+--
+
+CREATE EXTENSION IF NOT EXISTS postgis WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION postgis; Type: COMMENT; Schema: -; Owner:
+--
+
+COMMENT ON EXTENSION postgis IS 'PostGIS geometry, geography, and raster spatial types and functions';
+
+
+--
+-- Name: postgis_raster; Type: EXTENSION; Schema: -; Owner:
+--
+
+CREATE EXTENSION IF NOT EXISTS postgis_raster WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION postgis_raster; Type: COMMENT; Schema: -; Owner:
+--
+
+COMMENT ON EXTENSION postgis_raster IS 'PostGIS geometry, geography, and raster spatial types and functions';
+
+
+
 
 
 SET default_tablespace = '';
