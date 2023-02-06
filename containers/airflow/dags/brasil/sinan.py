4e758b348f8693e7036e97a588508701a0be5acc
Luã Bida Vacaro <luabidaa@gmail.com>
2023-02-06 15:14:04 -0300

    fix(sinan-dag): change pysus data path

--- containers/airflow/dags/brasil/sinan.py
+++ containers/airflow/dags/brasil/sinan.py
@@ -1,6 +1,7 @@
 import shutil
 import pendulum
 import logging as logger
+from pathlib import Path
 from datetime import timedelta
 from airflow.decorators import dag, task
 from airflow.operators.empty import EmptyOperator
@@ -30,7 +31,7 @@ default_args = {
 def brasil_sinan():
     """
     This DAG will fetch all diseases available on `diseases` from
-    SINAN FTP server. Data will be downloaded at `/tmp/pysus` and then
+    SINAN FTP server. Data will be downloaded at `$HOME/pysus` and then
     pushed into SQL database and the residues will be cleaned.
     @NOTE: This DAG can has a memory overhead that causes instability
            in Airflow System, therefore the max concurrency is set to 2.
@@ -53,25 +54,44 @@ def brasil_sinan():
 
         extract.download(disease)
 
-        logger.info(f"Data for {disease} downloaded at /tmp/pysus")
+        logger.info(f"Data for {disease} extracted")
 
 
     @task(task_id='upload')
     def upload_data():
         """
-        Creates table and upsert all data found in `/tmp/pysus/*.parquet`, 
-        cleaning after inserting
+        This task will upload every disease in the directory ~/pysus
+        that ends with `.parquet`, creating the corresponding table
+        to each disease in `brasil` schema. 
         """        
+        try:
+            loading.upload()
+        except Exception as e:
+            logger.error(e)
+            raise e
 
-        loading.upload()
+    @task(trigger_rule='all_done')
+    def remove_data_dir():
+        """ 
+        Cleans ~/pysus data directory.
+        """
+        pysus_data = Path.home() / 'pysus'
+        shutil.rmtree(pysus_data, ignore_errors=True)
+        logger.warning(f'{pysus_data} removed')
 
 
+    # `expand` will create a task for each disease/year.
+    # There will be about 35 diseases total, each disease can
+    # contain several years to fetch, setting `max_active_tasks`
+    # to 2 will allow only two diseases to be downloading at 
+    # the same time
     download = extract_data.expand(disease=list(diseases.keys()))
     
     upload = upload_data()
 
+    clean = remove_data_dir()
 
-    start >> download >> upload >> done 
+    start >> download >> upload >> done >> clean
 
 
 dag = brasil_sinan()
