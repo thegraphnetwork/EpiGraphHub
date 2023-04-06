3d3c3a33b9529548e820862fc3644a3e85b77277
Luã Bida Vacaro <luabidaa@gmail.com>
2023-04-06 09:33:13 -0300

    Touch .env at the host preparation

--- scripts/prepare-host.sh
+++ scripts/prepare-host.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+touch ${PROJECT_PATH}/.env
 
 PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd )"
 export $(echo $(cat ${PROJECT_PATH}/.env | sed 's/#.*//g'| xargs) | envsubst)
