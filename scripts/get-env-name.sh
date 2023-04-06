058ff1a884963aeef24bb41153d8c164d12677b8
Luã Bida Vacaro <luabidaa@gmail.com>
2023-04-06 10:24:34 -0300

    add -0 flag to xargs

--- scripts/get-env-name.sh
+++ scripts/get-env-name.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd )"
 
 . ${PROJECT_PATH}/scripts/load-dotenv.sh
 
--- scripts/load-dotenv.sh
+++ scripts/load-dotenv.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd )"
 
-export $(echo $(cat ${PROJECT_PATH}/.env | sed 's/#.*//g'| xargs) | envsubst)
+export $(echo $(cat ${PROJECT_PATH}/.env | sed 's/#.*//g'| xargs -0) | envsubst)
