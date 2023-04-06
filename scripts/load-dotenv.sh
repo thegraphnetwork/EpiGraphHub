26d386b8468b0349c3c5e38f1c764d92ec13db19
Luã Bida Vacaro <luabidaa@gmail.com>
2023-04-06 10:17:58 -0300

    Fix PROJECT_PATH

--- scripts/load-dotenv.sh
+++ scripts/load-dotenv.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd )"
+PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 export $(echo $(cat ${PROJECT_PATH}/.env | sed 's/#.*//g'| xargs) | envsubst)
