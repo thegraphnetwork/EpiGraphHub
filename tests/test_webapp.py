190ff59d01782c1cd25841591a7b47425ef670dc
Luã Bida Vacaro <luabidaa@gmail.com>
2023-05-17 14:51:49 -0300

    Add init file

--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import unittest
 
