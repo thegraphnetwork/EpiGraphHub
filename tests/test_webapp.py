8abfd98895cfce1fd1bee7dedc978a34d483d331
Luã Bida Vacaro <luabidaa@gmail.com>
2023-08-25 10:55:00 -0300

    Including driver path to selenium test

--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import unittest
