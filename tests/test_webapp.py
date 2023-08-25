f8c5508c0feed85347980de780cf6d1b6236b837
Luã Bida Vacaro <luabidaa@gmail.com>
2023-08-25 15:52:04 -0300

    include webdriver on PATH

--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -1,16 +1,12 @@
-import os
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 import unittest
 
 
 class TestEpiGraphHub(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        driver_path = os.getenv("CHROMEWEBDRIVER")
         chrome_options = Options()
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-gpu")
