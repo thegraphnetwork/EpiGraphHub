f18f4b83591a246ae0f9c46c26923dda5f36696b
Luã Bida Vacaro <luabidaa@gmail.com>
2023-08-25 11:26:06 -0300

    Add script to download google chrome on ci

--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -1,20 +1,27 @@
+import os
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 import unittest
-
 
 class TestEpiGraphHub(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
+        driver_path = os.getenv("CHROMEWEBDRIVER")
         chrome_options = Options()
+        chrome_options.add_argument("--no-sandbox");
         chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--disable-dev-shm-usage");
         chrome_options.add_argument("--headless")
-        chrome_path = ChromeDriverManager().install()
-        service = Service(executable_path=chrome_path)   
-        cls.driver = webdriver.Chrome(service=service)
+
+        if driver_path:
+            service = Service(executable_path=driver_path)   
+            cls.driver = webdriver.Chrome(service=service)
+        else:
+            cls.driver = webdriver.Chrome()
+
         cls.driver.get("http://localhost:8088")
 
     @classmethod
