9bcb602532519edadf9eac1210c36eea28209390
Luã Bida Vacaro <luabidaa@gmail.com>
2023-08-25 17:50:32 -0300

    try to fix chrome webdriver crash on CI

--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -7,10 +7,12 @@ class TestEpiGraphHub(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         chrome_options = Options()
-        chrome_options.add_argument("--no-sandbox");
-        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("start-maximized");
+        chrome_options.add_argument("disable-infobars");
+        chrome_options.add_argument("--disable-extensions");
+        chrome_options.add_argument("--disable-gpu");
         chrome_options.add_argument("--disable-dev-shm-usage");
-        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox");
 
         cls.driver = webdriver.Chrome()
 
