0fe44cfa168f381af3146a27ea698c0a41386f6b
Luã Bida Vacaro <luabidaa@gmail.com>
2023-08-28 08:41:10 -0300

    Try pinning selenium package

--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 import unittest
 
 
