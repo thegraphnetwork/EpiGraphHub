b47fdc92462750cc27a81d0a0ae9476b4e24524f
Luã Bida Vacaro <luabidaa@gmail.com>
2023-08-28 10:59:08 -0300

    fix(superset): minor fix on superset to use new credentials

--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -9,9 +9,7 @@ class TestEpiGraphHub(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         chrome_options = Options()
-        chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--headless")
         cls.driver = webdriver.Chrome(
             ChromeDriverManager().install(), options=chrome_options
