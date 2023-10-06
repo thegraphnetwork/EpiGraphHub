ef49a4f2f8dabaaac99217dbb9fc978598f73df5
Luã Bida Vacaro <luabidaa@gmail.com>
2023-10-06 09:54:03 -0300

    Trying to send information through external tasks

--- containers/airflow/dags/switzerland/foph_weekly_dag.py
+++ containers/airflow/dags/switzerland/foph_weekly_dag.py
@@ -1,6 +1,6 @@
 """
 @author Luã Bida Vacaro | github.com/luabida
-@date Last change on 2023-04-18
+@date Last change on 2023-10-05
 
 NOTE: This DAG is a modified copy of foph DAG to fetch weekly data.
       FOPH stopped updating the daily dataset for Covid in 2023,
@@ -8,18 +8,13 @@ NOTE: This DAG is a modified copy of foph DAG to fetch weekly data.
       took its place, keeping the same workflow with minor changes.
 """
 import pendulum
-import logging as logger
 
 from datetime import timedelta
-from airflow.decorators import dag, task
+from airflow import DAG
+from airflow.decorators import task
+from airflow.models import TaskInstance
 from airflow.operators.empty import EmptyOperator
-from airflow.operators.python import PythonOperator, BranchPythonOperator
-
-from epigraphhub.data.foph import (
-    extract,
-    loading,
-    transform,
-)
+from airflow.operators.python import ExternalPythonOperator, BranchExternalPythonOperator
 
 
 default_args = {
@@ -34,14 +29,13 @@ default_args = {
 }
 
 
-@dag(
+with DAG(
+    dag_id='FOPH_WEEKLY',
+    tags = ['CHE', 'FOPH', 'Switzerland'],
     schedule='@weekly',
     default_args=default_args,
     catchup=False,
-    tags = ['CHE', 'FOPH', 'Switzerland'],
-    max_active_tasks=6
-)
-def FOPH_WEEKLY():
+) as dag:
     start = EmptyOperator(
         task_id="start",
     )
@@ -51,17 +45,27 @@ def FOPH_WEEKLY():
         trigger_rule="all_success",
     )
 
-    tables_default = [[f'{t}_w', u] for t, u in extract.fetch(freq='weekly')]
-    tables_by_age = [[f'{t}_byage_w', u] for t, u in extract.fetch(freq='weekly', by='age')]
-    tables_by_sex = [[f'{t}_bysex_w', u] for t, u in extract.fetch(freq='weekly', by='sex')]
+    @task.external_python(
+        task_id='get_tables', python='/opt/py310/bin/python3.10'
+    )
+    def get_tables():
+        from epigraphhub.data.foph import extract
+        tables_default = [[f'{t}_w', u] for t, u in extract.fetch(freq='weekly')]
+        tables_by_age = [[f'{t}_byage_w', u] for t, u in extract.fetch(freq='weekly', by='age')]
+        tables_by_sex = [[f'{t}_bysex_w', u] for t, u in extract.fetch(freq='weekly', by='sex')]
 
-    tables = tables_default + tables_by_age + tables_by_sex
+        tables = tables_default + tables_by_age + tables_by_sex
+
 
     def download(url):
+        import logging as logger
+        from epigraphhub.data.foph import extract
         extract.download(url)
         logger.info(f"{str(url).split('/')[-1]} downloaded.")
 
     def compare(tablename, url):
+        import logging as logger
+        from epigraphhub.data.foph import loading
         filename = str(url).split("/")[-1]
 
         try:
@@ -76,32 +80,43 @@ def FOPH_WEEKLY():
         return f"{tablename}_up_to_date"
 
     def load_to_db(table, url):
+        import logging as logger
+        from epigraphhub.data.foph import loading
         filename = str(url).split("/")[-1]
         loading.upload(table, filename)
         logger.info(f"foph_{table} updated.")
 
     def parse_table(table):
+        import logging as logger
+        from epigraphhub.data.foph import transform
         transform.parse_date_region(table)
         logger.info(f"geoRegion and date index updated on foph_{table.lower()}")
 
-    @task(trigger_rule="all_done")
+    @task.external_python(
+        task_id='all_done', python='/opt/py310/bin/python3.10'
+    )
     def remove_csv_dir():
+        import logging as logger
+        from epigraphhub.data.foph import extract
         extract.remove(entire_dir=True)
         logger.info("CSVs directory removed.")
+
 
     for table in tables:
         tablename, url = table
 
-        down = PythonOperator(
+        down = ExternalPythonOperator(
             task_id=tablename,
             python_callable=download,
             op_kwargs={"url": url},
+            python='/opt/py310/bin/python3.10'
         )
 
-        comp = BranchPythonOperator(
+        comp = BranchExternalPythonOperator(
             task_id=f"comp_{tablename}",
             python_callable=compare,
             op_kwargs={"tablename": tablename, "url": url},
+            python='/opt/py310/bin/python3.10'
         )
 
         not_same_shape = EmptyOperator(
@@ -111,16 +126,18 @@ def FOPH_WEEKLY():
             task_id=f"{tablename}_up_to_date",
         )
 
-        load = PythonOperator(
+        load = ExternalPythonOperator(
             task_id=f"load_{tablename}",
             python_callable=load_to_db,
             op_kwargs={"table": tablename, "url": url},
+            python='/opt/py310/bin/python3.10'
         )
 
-        parse = PythonOperator(
+        parse = ExternalPythonOperator(
             task_id=f"parse_{tablename}",
             python_callable=parse_table,
             op_kwargs={"table": tablename},
+            python='/opt/py310/bin/python3.10'
         )
 
         done = EmptyOperator(
@@ -145,5 +162,3 @@ def FOPH_WEEKLY():
 
     clean = remove_csv_dir()
     end >> clean
-
-dag = FOPH_WEEKLY()
